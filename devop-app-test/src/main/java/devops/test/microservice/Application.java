22850052a10ca9e28aaed06465b16306fb2665fd
Ashish Prajapat <ashishprajapat2@gmail.com>
2018-01-05 13:06:05 +0530

    Test Commit

---
+++ devop-app-test/src/main/java/devops/test/microservice/Application.java
@@ -0,0 +1,34 @@
+package devops.test.microservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@SpringBootApplication
+
+public class Application {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        LOGGER.info("UForge Micro Service Started... Demo run");
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
---
+++ devop-app-test/src/main/java/devops/test/microservice/HelthCheckMicroService.java
@@ -0,0 +1,15 @@
+package devops.test.microservice;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class HelthCheckMicroService {
+
+	@RequestMapping("/")
+    public String home() {
+        return "devops test Service started.";
+    }
+}
